230fbf6de90c54cff5448b2c141e5a59512f684f
baumeisterfrank <baumeisterfrank@U11WKS06>
2018-01-25 14:27:04 +0100

    GUI

---
+++ WindowsFormsWWM/StartGUI.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsWWM
+{
+    partial class StartGUI
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartGUI));
+            this.buttonSpielStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSpielStart
+            // 
+            this.buttonSpielStart.Location = new System.Drawing.Point(392, 361);
+            this.buttonSpielStart.Name = "buttonSpielStart";
+            this.buttonSpielStart.Size = new System.Drawing.Size(283, 51);
+            this.buttonSpielStart.TabIndex = 0;
+            this.buttonSpielStart.Text = "Spielen";
+            this.buttonSpielStart.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::WindowsFormsWWM.Properties.Resources.logo;
+            this.pictureBox1.Image = global::WindowsFormsWWM.Properties.Resources.logo;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(349, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(371, 333);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // StartGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(994, 591);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonSpielStart);
+            this.Name = "StartGUI";
+            this.Text = "Who Wants To Be A Millionaire ?";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSpielStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
--- WindowsFormsWWM/StartGUI.cs
+++ WindowsFormsWWM/StartGUI.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsWWM
 {
-    public partial class Form1 : Form
+    public partial class StartGUI : Form
     {
-        public Form1()
+        public StartGUI()
         {
             InitializeComponent();
         }
