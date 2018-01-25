9d4bcf1daaa28ef01c51186e7a01aeed2567ff09
baumeisterfrank <baumeisterfrank@U11WKS06>
2018-01-25 15:26:31 +0100

    DADA

--- WindowsFormsWWM/Highscore.Designer.cs
+++ WindowsFormsWWM/Highscore.Designer.cs
@@ -28,32 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.labelHighscore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxHighscore = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // labelHighscore
             // 
-            this.button1.Location = new System.Drawing.Point(32, 66);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.labelHighscore.AutoSize = true;
+            this.labelHighscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHighscore.Location = new System.Drawing.Point(90, 19);
+            this.labelHighscore.Name = "labelHighscore";
+            this.labelHighscore.Size = new System.Drawing.Size(171, 39);
+            this.labelHighscore.TabIndex = 0;
+            this.labelHighscore.Text = "Highscore";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(308, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Die Highscore Liste aller Teilnehmer";
+            // 
+            // listBoxHighscore
+            // 
+            this.listBoxHighscore.BackColor = System.Drawing.Color.RoyalBlue;
+            this.listBoxHighscore.FormattingEnabled = true;
+            this.listBoxHighscore.Location = new System.Drawing.Point(92, 91);
+            this.listBoxHighscore.Name = "listBoxHighscore";
+            this.listBoxHighscore.Size = new System.Drawing.Size(639, 368);
+            this.listBoxHighscore.TabIndex = 2;
             // 
             // Highscore
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(797, 519);
+            this.Controls.Add(this.listBoxHighscore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelHighscore);
             this.Name = "Highscore";
             this.Text = "Highscore";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelHighscore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxHighscore;
     }
 }
