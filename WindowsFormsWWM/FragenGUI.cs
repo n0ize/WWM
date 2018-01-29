b77970e2fa33e4ce282f6bd48499be6d9ca383bc
baumeisterfrank <baumeisterfrank@U11WKS06>
2018-01-29 12:15:18 +0100

    vj

--- WindowsFormsWWM/FragenGUI.cs
+++ WindowsFormsWWM/FragenGUI.cs
@@ -7,22 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace WindowsFormsWWM
 {
     public partial class FragenGUI : Form
     {
         StartGUI sg;
+        OleDbDataReader dr;
+        OleDbConnection con;
         public FragenGUI(StartGUI sg)
         {
             InitializeComponent();
             this.sg = sg;
+            con = new OleDbConnection(Properties.Settings.Default.con);
+            con.Open();
         }
 
         private void buttonMenü_Click(object sender, EventArgs e)
         {
             sg.Visible = true;
             this.Visible = false;
+        }
+
+        private void check(OleDbDataReader dr)
+        {
+
         }
     }
 }
